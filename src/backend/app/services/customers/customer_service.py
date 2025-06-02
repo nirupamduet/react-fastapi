c808a179e0fea0bbab7b4d74085d0e74b5e59e0a
nirupam <nirupam.duet@gmail.com>
2025-06-03 00:44:45 +0600

    working for both

--- src/backend/app/services/customers/customer_service.py
+++ src/backend/app/services/customers/customer_service.py
@@ -12,12 +12,12 @@ def get_customer_by_email(db: Session, email: str):
     return db.query(Customer).filter(Customer.email == email).first()
 
 def get_customer_by_sap_id_or_domain_id(db: Session, id: str):
-    return db.query(Customer).filter(Customer.sap_id == id).first()
-    #if customer:
-    #    return customer
+    customer = db.query(Customer).filter(Customer.sap_id == id).first()
+    if customer:
+        return customer
     
     # If no customer found by SAP ID, check by domain ID
-    #return db.query(Customer).filter(Customer.domain_id == id).first()
+    return db.query(Customer).filter(Customer.domain_id == id).first()
 
 def create_customer(db: Session, customer: CustomerCreate):
     hashed_password = get_password_hash(customer.password)
